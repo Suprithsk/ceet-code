30971afc97e0b3754302d60f6d59f6279ca665bf
Suprith SK <139979177+suprithsk2001@users.noreply.github.com>
2025-03-08 17:50:54 +0530

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useEffect } from 'react'
 import './App.css'
-import questions from './assets/questions.json'
-import { useState } from 'react'
 import HomePage from './pages/HomePage'
-import { Question } from './types/types'
+
+
 function App() {
-  
-  const [data, setData] = useState<Question[]>([])
-  useEffect(() => {
-    setData(questions as unknown as Question[])
-    console.log(questions)
-  }, [])
   return (
     <section className=''>
         <HomePage />
