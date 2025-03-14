d56eb3a5c51d7f85a08cac80d595237a12dacda7
Suprith SK <139979177+suprithsk2001@users.noreply.github.com>
2025-03-14 21:48:54 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import HomePage from './pages/HomePage'
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
+    <>
+    <Analytics />
     <section className=''>
         <HomePage />
     </section>
+    </>
   )
 }
 
